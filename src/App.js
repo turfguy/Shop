6b2331ac1098432a7ab5fc2ef5a051f547882d5e
turfguy <ksm0811@ajou.ac.kr>
2022-05-14 00:02:14 +0900

    react-router 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import { Button,Navbar,Nav,Container,Row,Col } from 'react-bootstrap';
-import item1 from './img/item1.jpeg';
-import item2 from './img/item2.jpeg';
-import item3 from './img/item3.jpeg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import data from './data.js';
 import { useState } from 'react';
+import {Routes,Route,Link} from 'react-router-dom';
 
 function App() {
   let [belt] = useState(data);
@@ -14,52 +12,79 @@ function App() {
     <div className="App">
       <Navbar bg="light" variant="light">
         <Container>
-        <Navbar.Brand href="#home">민지홍똥꼬멍</Navbar.Brand>
+        <Link to="/"> 
+        <Navbar.Brand href="#home">BeltShop</Navbar.Brand>
+        </Link>
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#pricing">Cart</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg">
-      </div>
-      <br></br>
-      <h4>BEST ITEMS</h4>
-   
-            <Container>
+
+          <Routes>  
+          <Route path="/detail" element={<div>
+            <Detail  url={'item0.jpeg'} title={belt[0].title} brand={belt[0].brand} price={belt[0].price}/>   
+            </div>}/>
+          <Route path="/" element={
+          <div>
+              <div className="main-bg">
+                  </div>
+                <br></br>
+              <h4>BEST ITEMS</h4>
+                <Container>
                 <Row>
-                <Col sm>
-                  <ItemList  title={belt[0].title} brand={belt[0].brand} price={belt[0].price}/> 
-                </Col>
-                <Col sm>
-                <div className='item' style={{ backgroundImage: 'url('+ item2 +')'}}></div>
-                  <h4>{belt[1].title}</h4>
-                  <p>{belt[1].brand}</p>
-                  <p>{belt[1].price}</p>
-                </Col>
-                <Col sm>
-                  <div className='item' style={{ backgroundImage: 'url('+ item3 +')'}}></div>
-                  <h4>{belt[2].title}</h4>
-                  <p>{belt[2].brand}</p>
-                  <p>{belt[2].price}</p>
-                </Col>
-              </Row>
-          </Container>
-        
+                {
+                    data.map(function(a,i){
+                      return(
+                            <ItemList  url={'item'+i+'.jpeg'} title={belt[i].title} brand={belt[i].brand} price={belt[i].price}/> 
+                      )
+                        })
+                  }
+                </Row>
+                </Container>
+           </div>
+          }/>
+      </Routes> 
+      
+     
     
     </div>
     
   );
 }
 
+function Detail (props)
+{
+  return(
+        <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <img src={props.url} width="100%" />
+          </div>
+          <div className="col-md-6">
+            <h4 className="pt-5">{props.title}</h4>
+            <p>{props.brand}</p>
+            <p>{props.price}</p>
+            <button className="btn btn-danger">주문하기</button> 
+          </div>
+        </div>
+      </div> 
+  )
+}
 function ItemList (props)
 {
   return (
     <>
-      <div className='item' style={{ backgroundImage: 'url('+ item1 +')'}}> </div>
+      <Col sm>
+      <Link to="/detail">
+      <img className="item" src={props.url}/>
       <h4>{props.title}</h4>
       <p>{props.brand}</p>
       <p>{props.price}</p>
+      </Link>
+      </Col>
+
     </>
     )
 
