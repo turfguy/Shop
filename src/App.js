7a0291482ca8b48e9ee129331eb4c8fc40b83432
turfguy <ksm0811@ajou.ac.kr>
2022-05-13 22:01:39 +0900

    export/import

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import item1 from './img/item1.jpeg';
 import item2 from './img/item2.jpeg';
 import item3 from './img/item3.jpeg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import data from './data.js';
+import { useState } from 'react';
 
 function App() {
+  let [belt] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
         <Container>
-        <Navbar.Brand href="#home">라멘</Navbar.Brand>
+        <Navbar.Brand href="#home">민지홍똥꼬멍</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#pricing">Cart</Nav.Link>
@@ -21,28 +25,45 @@ function App() {
       </div>
       <br></br>
       <h4>BEST ITEMS</h4>
-      <Container>
-          <Row>
-          <Col sm>
-            <div className='item' style={{ backgroundImage: 'url('+ item1 +')'}}></div>
-            <h4>Lifting Belt</h4>
-            <p>Chriology</p>
-          </Col>
-          <Col sm>
-          <div className='item' style={{ backgroundImage: 'url('+ item2 +')'}}></div>
-          <h4>Leather Weight Belt</h4>
-            <p>CardilloUSA</p>
-          </Col>
-          <Col sm>
-            <div className='item' style={{ backgroundImage: 'url('+ item3 +')'}}></div>
-            <h4>Custom Weight Belt</h4>
-            <p>CardilloUSA</p>
-          </Col>
-        </Row>
-    </Container>
+   
+            <Container>
+                <Row>
+                <Col sm>
+                  <ItemList  title={belt[0].title} brand={belt[0].brand} price={belt[0].price}/> 
+                </Col>
+                <Col sm>
+                <div className='item' style={{ backgroundImage: 'url('+ item2 +')'}}></div>
+                  <h4>{belt[1].title}</h4>
+                  <p>{belt[1].brand}</p>
+                  <p>{belt[1].price}</p>
+                </Col>
+                <Col sm>
+                  <div className='item' style={{ backgroundImage: 'url('+ item3 +')'}}></div>
+                  <h4>{belt[2].title}</h4>
+                  <p>{belt[2].brand}</p>
+                  <p>{belt[2].price}</p>
+                </Col>
+              </Row>
+          </Container>
+        
+    
     </div>
     
   );
 }
+
+function ItemList (props)
+{
+  return (
+    <>
+      <div className='item' style={{ backgroundImage: 'url('+ item1 +')'}}> </div>
+      <h4>{props.title}</h4>
+      <p>{props.brand}</p>
+      <p>{props.price}</p>
+    </>
+    )
+
+}
+
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,23 @@
+let data = [
+    {
+      id : 0,
+      title : "Lifting Belt",
+      brand : "Chriology",
+      price : 120000
+    },
+  
+    {
+      id : 1,
+      title : "Leather Weight Belt",
+      brand : "CardilloUSA",
+      price : 110000
+    },
+  
+    {
+      id : 2,
+      title : "Custom Weight Belt",
+      brand : "CardilloUSA",
+      price : 130000
+    }
+  ]; 
+export default data;
