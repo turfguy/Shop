239acd33e9c8b9addd5c5f8b771fda053693f0d9
turfguy <ksm0811@ajou.ac.kr>
2022-05-18 18:21:55 +0900

    lifeCycle과useEffect 학습 + 타이머로 이벤트 팝업 띄우기

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,41 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 
 function Detail (props)
 {
+  let [alert,setAlert] = useState(0);
+  useEffect(()=>{
+
+    let a =  setTimeout(()=>{
+      setAlert(alert=1)
+    },2000)
+    return ()=>
+    {
+      clearTimeout(a);
+    }
+
+  });
+
+
+  let [count,setCount] = useState(0);
   let {id} = useParams();
   let belt = props.belt.find(function(x){
     return x.id == id
   });
   return(
         <>
-            <div className="container"> 
+            <div className="container">
+                  {
+                    alert == 0 ? 
+                    <>
+                    <br></br>
+                    <div className="alert alert-warning">
+                        2초 이내 구매시 할인!!  
+                    </div>
+                    </>              : null
+                  }              
                 <div className="row">
                   <div className="col-md-6">
                   <img src={"https://codingapple1.github.io/shop/shoes2.jpg"} width="100%" />
